dbfa851f517d70d0ae67690a3917cd51c47d94f0
Raj Malakpet <raj.malakpet@gmail.com>
2019-01-28 23:14:16 -0800

    app port changes are done

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var routes = require("./routes/routes");
 
 var app = express();
 
-var port = process.env.port || 8080;
-
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname+"/public"));
 
-app.listen(port, function () {
-    console.log('<=== app listening on port: '+port);   
-});
-
 app.get("/", routes.home);
 
 app.get("/:city", routes.city);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+    console.log('<=== app listening on port: '+port);   
+});
